0be3fd53751959334336680b26e2229d03889853
Oscar GP <oscarprograb@gmail.com>
2025-09-21 07:33:57 -0600

    feat(Balance): now it shows the metamask account and its balance

--- src/pages/Balance.tsx
+++ src/pages/Balance.tsx
@@ -1,3 +1,42 @@
-export default function Balance(){
-    return<>Balance</>
+import { useEffect, useState } from "react"
+import { ethers } from "ethers"
+
+
+export default function Balance() {
+    const [account, setAccount] = useState(null)
+    const [balance, setBalance] = useState(null)
+    useEffect(() => {
+        window.ethereum?.request({ method: "eth_requestAccounts" }).then((cuenta: any) => setAccount(cuenta[0]))?.catch(error => console.log(error))
+        console.log("useEffect execution")
+        const handleAccountsChanged = (accounts: any) => setAccount(accounts[0])
+        window.ethereum?.on("accountsChanged", handleAccountsChanged)
+        return () => window.ethereum?.removeListener("accountsChanged",handleAccountsChanged)
+    }, [window.ethereum])
+    useEffect(()=>{
+        if(account){
+            // window.ethereum?.request({
+            //     method: "eth_getBalance",
+            //     params: [account, "latest"]})
+            //     .catch(error => console.log(error))
+            //     .then((balance: any) => {
+            //     })
+            const provider = new ethers.BrowserProvider(window.ethereum)
+            provider.getBalance(account).then(b => setBalance(ethers.formatEther(b)))
+        }
+    },[account])
+    if (!window.ethereum) {
+        return <>No hay metamask</>
+    }
+    if (!account) {
+        return <>No hay cuenta</>
+    }
+    return <div>
+        <h2>Balance</h2>
+        <div>
+            {account ? account : "Cargando..."}
+        </div>
+        <div>
+            {balance}
+        </div>
+    </div>
 }
